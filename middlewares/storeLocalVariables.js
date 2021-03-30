54614596840d3574e1de8a84c23dfce61cfd900f
Sherry <sherry.c.liao@gmail.com>
2021-03-30 21:59:47 +0800

    feat: add navbar to show login status

--- middlewares/storeLocalVariables.js
+++ middlewares/storeLocalVariables.js
@@ -2,6 +2,7 @@ module.exports = {
   storeLocalVariables: (req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
+    res.locals.user = req.user
     next()
   }
 }
