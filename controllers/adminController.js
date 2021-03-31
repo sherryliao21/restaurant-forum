628c0f39d00dfd189cab88bfa48a00794f39b83d
Sherry <sherry.c.liao@gmail.com>
2021-03-31 11:54:45 +0800

    feat: add admin restaurant index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,13 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+
 const adminController = {
   getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+    return Restaurant.findAll({ raw: true })   // raw: true to turn sequelize object into JavaScript object
+      .then(restaurants => {
+        return res.render('admin/restaurants', { restaurants })
+      })
+      .catch(err => console.log(err))
   }
 }
 
