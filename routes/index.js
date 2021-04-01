ac37ca0094bc190c1d0c904a00ca3458733f0638
Sherry <sherry.c.liao@gmail.com>
2021-04-01 11:35:53 +0800

    feat: modify restaurant model and add multer

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const passport = require('passport')
 const adminController = require('../controllers/adminController')
 const restController = require('../controllers/restController')
 const userController = require('../controllers/userController')
+const multer = require('multer')    // file upload middleware
+const upload = multer({ dest: 'temp/' })   // upload to temp folder
 
 module.exports = (app) => {
 
@@ -29,11 +31,11 @@ module.exports = (app) => {
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
-  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
 
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
-  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
   app.get('/signup', userController.signUpPage)
