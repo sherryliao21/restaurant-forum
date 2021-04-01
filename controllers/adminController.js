e00485e8a18305cdef4d5cb761d48f8768d42268
Sherry <sherry.c.liao@gmail.com>
2021-04-01 15:33:57 +0800

    feat: add dotenv

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,7 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const fs = require('fs')
 const imgur = require('imgur-node-api')
-const IMGUR_CLIENT_ID = 'f1199c937cc9d82'
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
   getRestaurants: (req, res) => {
