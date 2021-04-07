0e8a1d9416259faabac63452814de1f14bf22db6
Sherry <sherry.c.liao@gmail.com>
2021-04-07 19:16:36 +0800

    feat: create category model and add restaurant.CategoryId and association

---
+++ migrations/20210407110900-add-categoryId-to-restaurant.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('Restaurants', 'CategoryId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('Restaurants', 'CategoryId', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+
+  }
+};
